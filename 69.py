8f3e81e290a9b041fc74fcb11eea82b739357252
137211ok <geroin1504@gmail.com>
2021-03-06 12:15:23 +0200

    initial commit

---
+++ 69.py
@@ -0,0 +1,24 @@
+import time
+print ( 'Здравствуйте, чтобы продолжть введите свои данные' )
+
+while true:
+    time.sleep(0.5)
+    n = input ('ваше имя:')
+    time.sleep(0.5)
+    s = inpun('ваша фамилия:')
+    time.sleep(0.5)
+    if pr == 'нет':
+           break
+full_name = ( n + " " +s )
+
+time.sleep(1)
+print( "подождите пожайлуств." )
+time.sleep(0.5)
+print( "подождте пожайлуста.." )
+time.sleep(0.5)
+print( "Подождите пожайлуста..." )
+time.sleep(1)
+elif full_name == ( "Андрей Ларионов" ):
+        print( "Проверка пройдена" )
+else:
+    print( "проверка не пройдена" )
